8a3ce7493c6c9116bbc130b4d8ebc58bc4738735
Edw. González <dev.zanntech@outlook.com>
2024-04-25 17:41:15 -0600

    Update Footer.tsx

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -22,17 +22,7 @@ export default function Footer() {
           "::before, ::after": { top: "0%" },
         }}
       >
-        <Typography color="grey.700" variant="h6" component="span">
-          Developed by{" "}
-          <Link
-            target="_blank"
-            underline="none"
-            sx={{ color: "text.primary" }}
-            href="https://github.com/crazy-man22"
-          >
-            Crazy Man
-          </Link>
-        </Typography>
+       
       </Divider>
     </Box>
   );
